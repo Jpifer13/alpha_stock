cd0e9cfde9d6dcc1ddc6a5ea9f1297d124cc5eb1
Jake <jakepifer3@gmail.com>
2019-10-22 18:56:12 -0400

    added more routes for crypto tech and timeseries

--- app/functions/read_only.py
+++ app/functions/read_only.py
@@ -1,22 +1,34 @@
 from app import apiKey
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
 # from pprint import pprint
 
 # import matplotlib.pyplot as plt
 
-def get_company_intraday(company_name):
+def get_company_timeseries(company_symbol, time_interval, out_size):
 
     ts = TimeSeries(key=apiKey, output_format='json')
-    # # companySymbol = input("What is the symbol of the company you are looking for?")
-    companySymbol = company_name
     
-    # # Get json object with the intraday data and another with  the call's metadata
-    # data, meta_data = ts.get_bbands(symbol=companySymbol,interval='60min', time_period=60)
+    # Get json object with the timeseries data and another with  the call's metadata
+    data, meta_data = ts.get_intraday(symbol=company_symbol, interval=time_interval, outputsize=out_size)
     
-    data, meta_data = ts.get_daily(symbol=companySymbol)
-    # # data.plot()
-    # # plt.title('Intraday Times Series for the ' + companySymbol + ' stock (60 min)')
-    # # plt.show()
+    return data, meta_data
+
+def get_company_techindicators(company_symbol, interval):
+
+    ts = TechIndicators(key=apiKey, output_format='json')
+    
+    # Get json object with the techindicator data and another with  the call's metadata
+    data, meta_data = ts.get_bbands(symbol=company_symbol,interval=interval)
+
+    return data, meta_data
+
+def get_crypto_data(crypto_symbol, market):
+
+    cc = CryptoCurrencies(key=apiKey, output_format='json')
+    
+    # Get json object with the crypto data and another with  the call's metadata
+    data, meta_data = cc.get_digital_currency_daily(symbol=crypto_symbol, market=market)
 
     return data, meta_data
--- app/routes.py
+++ app/routes.py
@@ -6,17 +6,38 @@ from app import app
 from app.functions.read_only import *
 from app.functions.read_write import *
 
-@app.route('/', methods=['GET'])
-def index():
+@app.route('/timeseries', methods=['GET'])
+def timeseries():
     """
     This is a route that grabs stock info for google
     """
 
-    data, meta_data = get_company_intraday('GOOGL')
+    data, meta_data = get_company_timeseries('GOOGL', '15min', 'full')
     
-    # print(data['2019-10-14 15:30']['Real Lower Band'])
-    # print(data['2019-10-14 15:30'])
     list_of_data = dict_to_list(data)
-    # print(list_of_data[0])
+
+    return render_template('mainpage.html', title='Stock App', list_of_data=list_of_data)
+
+@app.route('/crypto', methods=['GET'])
+def crypto():
+    """
+    This route grabs crypto data
+    """
+
+    data, meta_data = get_crypto_data('BTC', 'USD')
+    
+    list_of_data = dict_to_list(data)
+
+    return render_template('mainpage.html', title='Stock App', list_of_data=list_of_data)
+
+@app.route('/techindicators', methods=['GET'])
+def techindicators():
+    """
+    This route grabs crypto data
+    """
+
+    data, meta_data = get_company_techindicators('GOOGL', '60min')
+    list_of_data = dict_to_list(data)
+    print(list_of_data)
 
     return render_template('mainpage.html', title='Stock App', list_of_data=list_of_data)
