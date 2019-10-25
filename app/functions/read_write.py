e7d9a6de245c0a342aa2680d0051e00788a9dd5d
Jake <jakepifer3@gmail.com>
2019-10-24 21:17:07 -0400

    Added time column to data table

--- app/functions/read_write.py
+++ app/functions/read_write.py
@@ -1,8 +1,11 @@
 def dict_to_list(data):
     list_of_data = []
+    keys = []
     if(data):
         for key in data:
+            keys.append(key)
+        for key in data:
             list_of_data.append(data[key])
-        return(list_of_data)
+        return(list_of_data, keys)
     else:
         print("Data is empty. Cannot convert to list!")
